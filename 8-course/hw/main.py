7ee366d3b012041ac98c58421e6d79d5d80708e3
fractalshine <fractalshine@pm.me>
2022-05-05 00:54:39 +0300

    8 homework refactoring added main function

--- 8-course/hw/main.py
+++ 8-course/hw/main.py
@@ -3,38 +3,34 @@ from random import shuffle
 from question import *
 from utils import *
 
-questions = []
-answers = []
-score = 0
 
-q_dicts_list = load_questions()
-for q in q_dicts_list:
-    questions.append(Question(q['question'], q['answer'], q['difficulty']))
+def main():
+    questions = []
+    answers = []
+    score = 0
 
-shuffle(questions)
+    q_dicts_list = load_questions()
+    for q in q_dicts_list:
+        questions.append(Question(q['question'], q['answer'], q['difficulty']))
 
-for question in questions:
-    if not question.is_asked:
-        print(question.build_question())
-        question.is_asked = True
-        question.user_answer = input()
-        question.build_feedback()
-        # answers.append(question.is_correct())
-        if question.is_correct():
-            answers.append(True)
-            score += question.points
+    shuffle(questions)
+
+    for question in questions:
+        if not question.is_asked:
+            print(question.build_question())
+            question.is_asked = True
+            question.user_answer = input()
+            question.build_feedback()
+            if question.is_correct():
+                answers.append(True)
+                score += question.points
+            else:
+                answers.append(False)
         else:
-            answers.append(False)
+            continue
 
-    else:
-        continue
+    print(print_statistics(answers, score))
 
-print(print_statistics(answers, score))
 
-# rand_dict = random_by_num()
-# for q in q_dicts_list:
-#     rand_dict = random_by_num()
-#
-#     q1 = Question(rand_dict['question'], rand_dict['answer'], rand_dict['difficulty'])
-#
-#     print(q1.build_question())
+if __name__ == "__main__":
+    main()
