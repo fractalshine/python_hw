2e4109bbdde9c1db1be83c49ae53586ec8eaf1a4
fractalshine <fractalshine@pm.me>
2022-05-13 22:39:02 +0300

    in 10 hw added candidates route

--- 10-course/hw/main.py
+++ 10-course/hw/main.py
@@ -3,10 +3,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+candidates_list = load_candidates()
+
 
 @app.route("/")
 def page_index():
-    candidates_list = load_candidates()
     string = ""
     for dictionary in candidates_list:
         string += "Имя кандидата - " + dictionary['name'] + "\n" + \
@@ -14,6 +15,25 @@ def page_index():
                   "Навыки через запятую: " + dictionary['skills'] + "\n" + \
                   "" + "\n"
     return f'{"<pre>" + string + "</pre>"}'
+
+
+@app.route("/candidates/<int:uid>")
+def page_uid(uid):
+    string = ""
+    pic = ""
+    for dictionary in candidates_list:
+        if uid == dictionary['id']:
+            string += "Имя кандидата - " + dictionary['name'] + "\n" + \
+                      "Позиция кандидата - " + dictionary['position'] + "\n" + \
+                      "Навыки через запятую: " + dictionary['skills'] + "\n" + \
+                      "" + "\n"
+            pic = dictionary['picture']
+    return f'<img src="{pic}">' \
+           f'{"<pre>" + string + "</pre>"}'
+
+
+
+
 
 
 app.run()
