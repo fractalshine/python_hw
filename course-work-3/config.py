e8a35c5e01af8af2ad55134da85a4c880797b405
fractalshine <fractalshine@pm.me>
2022-07-01 22:14:47 +0300

    course work 3 added

---
+++ course-work-3/config.py
@@ -0,0 +1,20 @@
+import logging
+
+# path config
+POST_PATH = "data/data.json"
+COMMENTS_PATH = "data/comments.json"
+BOOKMARKS_PATH = "data/bookmarks.json"
+UPLOAD_FOLDER = "uploads/images"
+LOGS_PATH = "logs/basic.log"
+
+# ext conf
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
+
+# logs conf
+# logging.basicConfig(filename=LOGS_PATH)
+logger = logging.getLogger("logger")
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(LOGS_PATH)
+log_formatter = logging.Formatter("%(levelname)s : %(asctime)s : %(message)s")
+file_handler.setFormatter(log_formatter)
+logger.addHandler(file_handler)
