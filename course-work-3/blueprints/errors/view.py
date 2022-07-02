b2f4cb4d610ff7b777804fdf991cc1497e92d9c5
fractalshine <fractalshine@pm.me>
2022-07-02 22:53:26 +0300

    course work 3 fixed css

--- course-work-3/blueprints/errors/view.py
+++ course-work-3/blueprints/errors/view.py
@@ -1,4 +1,6 @@
 from flask import render_template, Blueprint
+import json
+from config import logger
 
 # Добавим настройку директории с шаблонами
 error_blueprint = Blueprint(
@@ -8,10 +10,24 @@ error_blueprint = Blueprint(
 
 
 @error_blueprint.app_errorhandler(413)
-def page_not_found(e):
+def file_too_big(e):
     return f'File to big: {e}'
 
 
 @error_blueprint.app_errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
+@error_blueprint.app_errorhandler(json.JSONDecodeError)
+def database_corrupted(e):
+    message = "Бд повреждена"
+    logger.error(f"{message} - {e}")
+    return render_template("index_errorpage.html", message=message)
+
+
+@error_blueprint.app_errorhandler(FileNotFoundError)
+def database_missed(e):
+    message = "Бд отсутствует"
+    logger.error(f"{message} - {e}")
+    return render_template("index_errorpage.html", message=message)
--- course-work-3/blueprints/main/views.py
+++ course-work-3/blueprints/main/views.py
@@ -2,7 +2,7 @@
 
 # Добавим импорт шаблонизатора
 from flask import render_template, Blueprint, request
-from utils import validate_json, get_posts_by_word
+from utils import load_posts, get_posts_by_word
 
 # Добавим настройку папки с шаблонами
 main_blueprint = Blueprint(
@@ -15,8 +15,8 @@ main_blueprint = Blueprint(
 # Добавим render_template
 @main_blueprint.route('/')
 def main_page():
-    template, text, posts = validate_json()
-    return render_template(template, posts=posts, text=text)
+    posts = load_posts()
+    return render_template("index.html", posts=posts)
 
 
 # Добавим render_template
